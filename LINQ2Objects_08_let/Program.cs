9d890b20231ea8c6412c7075be82ad791146be70
Marco Happe <marco.happe@outlook.com>
2021-10-06 15:26:03 +0200

    final commit

--- LINQ2Objects_08_let/Program.cs
+++ LINQ2Objects_08_let/Program.cs
@@ -29,22 +29,60 @@ namespace LINQ2Objects_08_let
             };
 
 
-            // Alle Studenten ermitteln deren Gesamtpunkteanzahl > 300 ist
+            // Alle Studenten ermitteln deren Gesamtpunkteanzahl >= 300 ist
             // -----------------------------------------------------------
 
-            //var q1 = from s in studenten
+            var q1 = from s in studenten
+                         //let gesamtPunkte = s.Punkte[0] + s.Punkte[1] + s.Punkte[2] + s.Punkte[3]
+
+                         // Einfacher und unabhängig von der Anzahl der Listenelemente wäre
+
+                     let gesamtPunkte = s.Punkte.Sum()
+                     where gesamtPunkte >= 300
+                     orderby gesamtPunkte descending
+                     select new
+                     {
+                         Name = s.Vorname + " " + s.Nachname,
+                         Gesamtpunkte = gesamtPunkte
+                     };
+
+            Console.WriteLine("Studenten mit Gesamtpunktzahl >= 300:");
+            foreach (var item in q1)
+            {
+                Console.WriteLine($"{item.Name,-25} {item.Gesamtpunkte,3} Punkte");
+            }
+            Console.WriteLine();
 
 
+            // Von Christian Morgenstern
+            string[] gedicht =
+            {
+               "Ein Wiesel saß auf einem Kiesel inmitten Bachgeriesel.",
+               "Wisst ihr weshalb?",
+               "Das Mondkalb verriet es mir im stillen:",
+               "Das raffinierte Tier tat's um des Reimes willen."
+            };
 
 
+            // Alle Wörter in einem Gedicht ermitteln die mit einem Vokal beginnen
+            // -------------------------------------------------------------------
 
-            //            string[] gedicht =
-            //            {
-            //   "Ein Wiesel saß auf einem Kiesel inmitten Bachgeriesel.",
-            //   "Wisst ihr weshalb?",
-            //   "Das Mondkalb verriet es mir im stillen:",
-            //   "Das raffinierte Tier tat's um des Reimes willen."
-            //};
+            var q2 = from zeile in gedicht
+                     let woerter = zeile.Split(' ')
+                     from wort in woerter
+                     let w = wort.ToLower()
+                     where "aeiou".Contains(w[0])
+                     // Könnte man auch ohne Variable w auch wie folgt schreiben
+                     //where "aeiou".Contains(wort.ToLower()[0])
+                     select wort;
+
+            Console.WriteLine("Alle Wörter die mit einem Vokal beginnen:");
+            foreach (string wort in q2)
+            {
+                Console.WriteLine(wort);
+            }
+            Console.WriteLine();
+
 
             // Übung:
             // Alle Studenten in 100er-Gesamtpunktezahlgruppen (0-99, 100-199, 200-299, ...)
@@ -69,10 +107,28 @@ namespace LINQ2Objects_08_let
             // -----------------------------------------------------------------------------
 
 
+            Console.WriteLine("Alle Studenten in 100er-Gesamtpunktezahlgruppen:");
+
+            var q3 = from s in studenten
+                     let gesamtPunkte = s.Punkte.Sum()
+                     orderby gesamtPunkte descending        // Sortierung innerhalb der Gruppe
+                     group new
+                     {
+                         Name = s.Vorname + " " + s.Nachname,
+                         Gesamtpunkte = gesamtPunkte
+                     }
+                     by gesamtPunkte / 100 into gruppen
+                     select gruppen;
 
 
-
-
+            foreach (var gruppe in q3)
+            {
+                Console.WriteLine("\n" + gruppe.Key + "00 - " + gruppe.Key + "99:");
+                foreach (var item in gruppe)
+                {
+                    Console.WriteLine($"    {item.Name,-25} {item.Gesamtpunkte,3} Punkte");
+                }
+            }
 
 
 
